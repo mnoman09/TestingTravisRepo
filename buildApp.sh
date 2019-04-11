7e41112378a1749d91bead89d58516a94bd8592b
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-04-11 20:03:46 +0500

    removed extra lines

--- buildApp.sh
+++ buildApp.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-ls "C:\Windows\Microsoft.NET\Framework"
 cd ./travisIntegrationTesting
 "C:\Windows\Microsoft.NET\Framework\v4.0.30319\Msbuild.exe" ./travisIntegrationTesting/travisIntegrationTesting.csproj
-
-cd C:
-ls windows/Microsoft.NET/
-ls "C:\Program Files (x86)\MSBuild\14.0\Bin"
