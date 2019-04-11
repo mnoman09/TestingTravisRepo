e687d02544ff35f0057b0009be54a045b16b049d
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-04-11 19:27:32 +0500

    dir structutre

--- buildApp.sh
+++ buildApp.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -ex
 
+ls "C:\Windows\Microsoft.NET\Framework"
 cd ./travisIntegrationTesting
-"C:\Program Files (x86)\MSBuild\14.0\Bin\Msbuild.exe" ./travisIntegrationTesting/travisIntegrationTesting.csproj
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Msbuild.exe" ./travisIntegrationTesting/travisIntegrationTesting.csproj
 
 cd C:
 ls windows/Microsoft.NET/
