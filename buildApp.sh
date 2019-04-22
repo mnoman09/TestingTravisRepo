6d403afe2075c02cbae93ac2b62d2f402b2f04e9
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-04-22 20:43:40 +0500

    added publish on build

--- buildApp.sh
+++ buildApp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Msbuild.exe" ./SampleWebApi/SampleWebApi.sln
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Msbuild.exe" ./SampleWebApi/SampleWebApi.sln /p:DeployOnBuild=true /p:PublishProfile=Release
 ls ./SampleWebApi/SampleWebApi
 ./dockerbuild.sh
