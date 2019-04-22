3c9a460315b0ab9a3060f1a24a0bb994559e8ea1
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-04-22 20:49:51 +0500

    ls

--- buildApp.sh
+++ buildApp.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Msbuild.exe" ./SampleWebApi/SampleWebApi.sln
+ls ./
+"C:\Windows\Microsoft.NET\Framework\v4.0.30319\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
 ls ./SampleWebApi/SampleWebApi
 ./dockerbuild.sh
