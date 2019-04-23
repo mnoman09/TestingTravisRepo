046e511fe07068e75469c139cacc6a23b82efff7
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-04-23 20:55:17 +0500

    updated msbuild path

--- buildApp.sh
+++ buildApp.sh
@@ -3,8 +3,7 @@ set -ex
 
 ls ./SampleWebApi
 echo "listing msBuild content"
-ls "C:\Program Files (x86)\MSBuild"
 
-"C:\Program Files (x86)\MSBuild\15.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
 ls ./SampleWebApi/SampleWebApi
 ./dockerbuild.sh
