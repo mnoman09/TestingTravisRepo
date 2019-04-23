403d498ba6867e8dc9c8724a6e14682290b76018
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-04-23 21:04:20 +0500

    listing contetn

--- buildApp.sh
+++ buildApp.sh
@@ -4,6 +4,8 @@ set -ex
 ls ./SampleWebApi
 echo "listing msBuild content"
 ls "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild"
+echo "content of ...."
+ls "C:\Program Files (x86)\Microsoft Visual Studio"
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
 ls ./SampleWebApi/SampleWebApi
 ./dockerbuild.sh
