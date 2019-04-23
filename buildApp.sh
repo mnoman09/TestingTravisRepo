37ded10a3ea5325ff6b10fe9b0f9dc3b7d20f756
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-04-23 16:55:29 +0500

    up

--- buildApp.sh
+++ buildApp.sh
@@ -2,8 +2,9 @@
 set -ex
 
 ls ./SampleWebApi
-ls "C:\Program Files (x86)"
+echo "listing msBuild content"
 ls "C:\Program Files (x86)\MSBuild"
-"C:\Program Files (x86)\MSBuild\14.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
+
+"C:\Program Files (x86)\MSBuild\15.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
 ls ./SampleWebApi/SampleWebApi
 ./dockerbuild.sh
