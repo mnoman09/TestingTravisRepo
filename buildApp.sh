51b136d8aad31ef1c5f696ba083a642cea152e71
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-04-23 16:52:12 +0500

    s

--- buildApp.sh
+++ buildApp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-ls ./SampleWebApi/bin/
+ls ./SampleWebApi
 ls "C:\Program Files (x86)"
 ls "C:\Program Files (x86)\MSBuild"
 "C:\Program Files (x86)\MSBuild\14.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
