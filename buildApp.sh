bdde59ba3887f3a2d6e9c2afc42f46984079eab2
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-05-02 22:35:03 +0500

    moved lines up

--- buildApp.sh
+++ buildApp.sh
@@ -6,9 +6,11 @@ echo "listing msBuild content"
 ls "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild"
 echo "content of ...."
 ls "C:\Program Files (x86)\Microsoft Visual Studio"
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
-ls ./SampleWebApi/SampleWebApi
+
 echo "installing 2017 agent"
 choco install visualstudio2017testagents
 choco upgrade visualstudio2017testagent
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
+ls ./SampleWebApi/SampleWebApi
 ./dockerbuild.sh
