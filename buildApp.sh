0594369627a26a665815f8a1219ea9f57ee399a2
FOLIO3PK\muhammadnoman <muhammadnoman@folio3.com>
2019-05-02 22:32:57 +0500

    added log

--- buildApp.sh
+++ buildApp.sh
@@ -8,6 +8,7 @@ echo "content of ...."
 ls "C:\Program Files (x86)\Microsoft Visual Studio"
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\Msbuild.exe" ./SampleWebApi/SampleWebApi.csproj
 ls ./SampleWebApi/SampleWebApi
+echo "installing 2017 agent"
 choco install visualstudio2017testagents
 choco upgrade visualstudio2017testagent
 ./dockerbuild.sh
